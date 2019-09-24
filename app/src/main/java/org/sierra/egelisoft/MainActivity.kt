a6589856ed5bf07cbd0ea77b6b6782810a0b1f31
Juan Carlos <rapidclimate@gmail.com>
2019-09-24 16:37:55 -0500

    aaaa

--- app/src/main/java/org/sierra/egelisoft/MainActivity.kt
+++ app/src/main/java/org/sierra/egelisoft/MainActivity.kt
@@ -16,27 +16,7 @@ lateinit var  containerView:ViewGroup
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+      supportActionBar?.hide()
 
-        button.setOnClickListener {
-         var valorJson=  application.assets.open("temas.json").bufferedReader().use {
-               it.readLine()
-           }
-            val moshi: Moshi = Moshi.Builder().build()
-            val adapter: JsonAdapter<Array<Temas>> = moshi.adapter(Array<Temas>::class.java)
-            val preguntas = adapter.fromJson(valorJson)
-            val pregunta=preguntas?.get(0)
-
-
-
-            Toast.makeText(this, "Pero es Pregunta ${pregunta?.area} ", Toast.LENGTH_LONG).show()
-
-var media= MediaPlayer()
-            var descriptor=application.assets.openFd("sad.mp3")
-            media.setDataSource(descriptor.fileDescriptor,descriptor.startOffset,descriptor.length)
-            media.prepare()
-            media.start()
-
-
-        }
     }
 }
