c77bc91a15ab2738072159c4df4802dc6079ec92
Juan Carlos <rapidclimate@gmail.com>
2019-10-03 14:03:35 -0500

    Yo soy topoyiyo

---
+++ app/src/main/java/org/sierra/egelisoft/AlumnoEntity.kt
@@ -0,0 +1,11 @@
+package org.sierra.egelisoft
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName = "alumno")
+data class  AlumnoEntity(@PrimaryKey @ColumnInfo(name="id") val id:String=UUID.randomUUID().toString(),
+                         @ColumnInfo(name = "nombre")val nombre:String,
+                         @ColumnInfo(name="email")val email:String)
